823b24ff013339011b9ca92c488903baabeec8da
rohin4u <58321982+rohin4u@users.noreply.github.com>
2021-12-18 07:59:39 +0530

    Initial commit

---
+++ src/com/spiderscrawl/WrapperDemo.java
@@ -0,0 +1,45 @@
+package com.spiderscrawl;
+
+public class WrapperDemo {
+
+	public static void main(String[] args) {
+		
+		Integer i = new Integer(10);
+		System.out.println(i);
+		
+		Integer i1 = new Integer("10");
+		System.out.println(i1);
+		
+//		Integer i2 = new Integer("34");
+//		System.out.println(i2);
+//		
+//		Float f = new Float("india");
+//		System.out.println(f);
+		
+//		Boolean b = new Boolean(true);
+//		Boolean b1 = new Boolean("true");
+//		Boolean b2 = new Boolean("True");
+//		Boolean b3= new Boolean("TRUE");
+//		Boolean b4 = new Boolean("Dog");
+//		System.out.println(b4);
+//		
+//		Integer i4 = Integer.valueOf(10);
+//		System.out.println(i4);
+		
+//		Integer i5 = Integer.valueOf("1010001", 2);
+//		System.out.println(i5);
+		
+//		Integer i6 = Integer.valueOf(1000);
+//		System.out.println(i6.intValue());
+//		System.out.println(i6.floatValue());
+//		System.out.println(i6.shortValue());
+		
+		
+		Integer i8 = Integer.parseInt("delhi");
+		System.out.println(i8);
+		""
+		
+
+	}
+
+}
